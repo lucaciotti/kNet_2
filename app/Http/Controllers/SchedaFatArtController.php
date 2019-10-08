42a4b1fd464cac0a2f74b2d6ab730a3d442700af
lucaciotti <luca.ciotti@gmail.com>
2019-10-08 09:50:11 +0200

    Updated Stat Confr. Anni XLS List Cli

--- app/Http/Controllers/SchedaFatArtController.php
+++ app/Http/Controllers/SchedaFatArtController.php
@@ -10,6 +10,7 @@ use knet\ArcaModels\Client;
 use knet\ArcaModels\Agent;
 use knet\ArcaModels\Product;
 use knet\Exports\StatFatArtExport;
+use knet\Exports\StatFatArtListCliExport;
 use knet\Helpers\PdfReport;
 use Maatwebsite\Excel\Facades\Excel;
 
@@ -430,6 +431,7 @@ class SchedaFatArtController extends Controller
         }
         $fatList->groupBy('codicecf');
         $fatList->havingRaw('fatN >= ?', [$limitVal]);
+        $fatList->orderBy('fatN', 'desc');
         $fatList = $fatList->get();
         // dd($fatList->toSql());
 
@@ -451,6 +453,68 @@ class SchedaFatArtController extends Controller
         $pdf = PdfReport::A4Landscape($view, $data, $title, $subTitle);
 
         return $pdf->stream($title . '-' . $subTitle . '.pdf');
+    }
+
+    public function downloadXLSListaCli(Request $req, $codAg = null)
+    {
+        $agentList = Agent::select('codice', 'descrizion')->whereNull('u_dataini')->orderBy('codice')->get();
+        $codAg = ($req->input('codag')) ? $req->input('codag') : $codAg;
+        $fltAgents = (!empty($codAg)) ? $codAg : array_wrap((!empty(RedisUser::get('codag')) ? RedisUser::get('codag') : $agentList->first()->codice));
+        $thisYear = (Carbon::now()->year);
+        $settoreSelected = ($req->input('settoreSelected')) ? $req->input('settoreSelected') : null;
+        $zoneSelected = ($req->input('zoneSelected')) ? $req->input('zoneSelected') : null;
+        $yearBack = ($req->input('yearback')) ? $req->input('yearback') : 3; // 2->3AnniView; 3->4AnniView; 4->5AnniView
+        $limitVal = ($req->input('limitVal') || $req->input('limitVal') == '0') ? $req->input('limitVal') : 500;
+        $meseSelected = $req->input('mese');
+        $onlyMese = $req->input('onlyMese') ? $req->input('onlyMese') : false;
+        $isPariPeriodo = $onlyMese ? $onlyMese : ($req->input('pariperiodo') ? $req->input('pariperiodo') : false);
+        // dd($req->input('limitVal'));
+
+        $querySelect_fat = $meseSelected ? $this->buildQueryPeriodo('u_statfatt_art.val_', intval($meseSelected), $onlyMese) : 'u_statfatt_art.val_tot';
+        $querySelect_fatN = ($isPariPeriodo ? $querySelect_fat : 'u_statfatt_art.val_tot');
+
+        // Qui costruisco solo la tabella con il fatturato dei clienti
+        $fatList = DB::connection(RedisUser::get('ditta_DB'))->table('u_statfatt_art')
+            ->join('anagrafe', 'anagrafe.codice', '=', 'u_statfatt_art.codicecf')
+            ->leftJoin('settori', 'settori.codice', '=', 'anagrafe.settore')
+            ->select('u_statfatt_art.codicecf')
+            ->selectRaw('MAX(anagrafe.descrizion) as ragionesociale, MAX(settori.descrizion) as settore, MIN(u_statfatt_art.mese_parz) as meseRif')
+            ->selectRaw('SUM(IF(u_statfatt_art.esercizio = ?,' . $querySelect_fat . ', 0)) as fatN', [$thisYear])
+            ->selectRaw('SUM(IF(u_statfatt_art.esercizio = ?,' . $querySelect_fatN . ', 0)) as fatN1', [$thisYear - 1])
+            ->selectRaw('SUM(IF(u_statfatt_art.esercizio = ?,' . $querySelect_fatN . ', 0)) as fatN2', [$thisYear - 2]);
+
+        switch ($yearBack) {
+            case 3:
+                $fatList->selectRaw('SUM(IF(u_statfatt_art.esercizio = ?,' . $querySelect_fatN . ', 0)) as fatN3', [$thisYear - 3]);
+                break;
+            case 4:
+                $fatList->selectRaw('SUM(IF(u_statfatt_art.esercizio = ?,' . $querySelect_fatN . ', 0)) as fatN3', [$thisYear - 3]);
+                $fatList->selectRaw('SUM(IF(u_statfatt_art.esercizio = ?,' . $querySelect_fatN . ', 0)) as fatN4', [$thisYear - 4]);
+                break;
+        }
+        // $fatList->whereRaw('anagrafe.agente = ? AND LENGTH(anagrafe.agente) = ?', [$agente, strlen($agente)]);
+        $fatList->whereIn('anagrafe.agente', $fltAgents);
+        $fatList->whereRaw('(LEFT(u_statfatt_art.codicearti,4) != ? AND LEFT(u_statfatt_art.codicearti,4) != ? AND LEFT(u_statfatt_art.codicearti,4) != ?)', ['CAMP', 'NOTA', 'BONU']);
+        $fatList->whereRaw('(LEFT(u_statfatt_art.gruppo,1) != ? AND LEFT(u_statfatt_art.gruppo,1) != ? AND LEFT(u_statfatt_art.gruppo,3) != ?)', ['C', '2', 'DIC']);
+        if ($settoreSelected != null) $fatList->whereIn('anagrafe.settore', $settoreSelected);
+        if ($zoneSelected != null) $fatList->whereIn('anagrafe.zona', $zoneSelected);
+        if ($req->input('grpPrdSelected')) {
+            $fatList->whereIn('u_statfatt_art.gruppo', $req->input('grpPrdSelected'));
+        }
+        if (!empty($req->input('optTipoProd'))) {
+            $fatList->where('u_statfatt_art.prodotto', $req->input('optTipoProd'));
+        }
+        $fatList->groupBy('codicecf');
+        $fatList->havingRaw('fatN >= ?', [$limitVal]);
+        $fatList->orderBy('fatN', 'desc');
+        $fatList = $fatList->get();
+        // dd($fatList->toSql());
+
+        $meseRif = $meseSelected ? $meseSelected : ($fatList->first() ? $fatList->first()->meseRif : Carbon::now()->month);
+
+        // dd($fatList->toSql());
+
+        return Excel::download(new StatFatArtListCliExport($fatList, $thisYear, $yearBack, $meseRif, $onlyMese, $isPariPeriodo), 'ListaCli_ConfrAnni.xlsx');
     }
 
 
