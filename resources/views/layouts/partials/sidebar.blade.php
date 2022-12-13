3ab09ee1e1af139c450ebd0f74739e7b08f8bbb7
Luca Ciotti <luca.ciotti@gmail.com>
2022-12-13 18:38:52 +0100

    Ultimo fix permessi Marketing

--- resources/views/layouts/partials/sidebar.blade.php
+++ resources/views/layouts/partials/sidebar.blade.php
@@ -54,7 +54,7 @@
                         <li class="{{ Ekko::isActiveRoute('listini::idxCli') }}"><a href="{{ route('listini::idxCli') }}">Cliente</a></li>
                         <li class="{{ Ekko::isActiveRoute('listini::grpCli') }}"><a href="{{ route('listini::grpCli') }}">Gruppo Clienti</a></li>
                         <li>&nbsp;</li>
-                        @if (!in_array(RedisUser::get('role'), ['agent', 'client']))
+                        @if (!in_array(RedisUser::get('role'), ['agent', 'client', 'marketing']))
                             <li class="{{ Ekko::isActiveRoute('listini::grpCli') }}"><a href="{{ route('listini::cliListScad') }}">Listino Clienti in Scadenza</a></li>
                             <li class="{{ Ekko::isActiveRoute('listini::grpCli') }}"><a href="{{ route('listini::grpListScad') }}">Listino Gruppi in Scadenza</a></li>
                         @endif
@@ -64,8 +64,10 @@
                         @endif --}}
                     </ul>
                 </li>
-                @endif
+              @endif
+              @if (!in_array(RedisUser::get('role'), ['marketing']))
               <li class="{{ Ekko::isActiveRoute('scad::*') }}"><a href="{{ route('scad::list') }}"><i class='fa fa-money'></i> <span>{{ trans('_menu.payment') }}</span></a></li>
+              @endif
               {{-- <li class="{{ Ekko::isActiveRoute('prod::*') }}"><a href="{{ route('prod::list') }}"><i class='fa fa-cube'></i> <span>{{ trans('_menu.products') }}</span></a></li> --}}
               <li><i class='fa fa-empty'></i></li>
 
