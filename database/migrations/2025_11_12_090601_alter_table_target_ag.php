54273615d890ba3457f19bd4f0ed20e80b65b8db
Luca Ciotti <luca.ciotti@gmail.com>
2025-11-12 10:07:52 +0100

    Fix: Migraztion table u_targetag

---
+++ database/migrations/2025_11_12_090601_alter_table_target_ag.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AlterTableTargetAg extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('u_targetag', function (Blueprint $table) {
+            $table->float('premio_valuta')->nullable()->default(0.0);
+            $table->string('premio_perc', 255)->nullable()->default('');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('u_targetag', function (Blueprint $table) {
+            $table->dropColumn(['premio_valuta', 'premio_perc']);
+        });
+    }
+}
